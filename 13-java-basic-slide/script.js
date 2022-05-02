41a4123e0e45e2c9a7a33e6743cd6de3c4c873da
Eunseo Choi <yses9296@gmail.com>
2022-05-02 17:11:58 +0900

    javascript 15

--- 13-java-basic-slide/script.js
+++ 13-java-basic-slide/script.js
@@ -36,5 +36,56 @@ for (var i = 0; i < slideCount; i++){
 }
 
 //슬라이드 이동 함수
-navPrev.addEventListener('click', function(){});
-navNext.addEventListener('click', function(){});
+function goToSlide(idx){
+    slideContainer[0].style.left = idx*(-100)+"%";
+    slideContainer[0].classList.add('animated');
+    currentIndex = idx;
+
+    // updateNav();
+
+}
+
+//버튼 기능 업데이트 함수
+function updateNav(){
+    if(currentIndex == 0){
+        navPrev.classList.add('disabled');
+    }
+    else{
+        navPrev.classList.remove('disabled');
+    }
+
+    if(currentIndex == slideCount-1){
+        navNext.classList.add('disabled');
+    }
+    else{
+        navNext.classList.remove('disabled');
+    }
+}
+
+//버튼을 클릭하면 슬라이드 이동시키기
+navPrev.addEventListener('click', function(e){
+    e.preventDefault();
+    // goToSlide(currentIndex -1);
+    //처음이 아니라면, 이전으로 이동.. >> goToSlide(currentIndex -1);
+    //처음이라면 >> goToSlide(????);
+
+    if(currentIndex > 0){
+        goToSlide(currentIndex -1);
+    }
+    else{
+        goToSlide(slideCount-1);
+    }
+});
+navNext.addEventListener('click', function(e){
+    e.preventDefault();
+    // goToSlide(currentIndex + 1);
+    if(currentIndex < slideCount-1){
+        goToSlide(currentIndex +1);
+    }
+    else{
+        goToSlide(0);
+    }
+});
+
+//첫 번째 슬라이드 먼저 보이도록 하기
+goToSlide(0);
