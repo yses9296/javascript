25d7c0ccf2797153c3e6081bd4d485c11a268157
Eunseo Choi <yses9296@gmail.com>
2022-05-14 19:18:00 +0900

    javascript 52

---
+++ 52-java-multiple-slideshow/script.js
@@ -0,0 +1,71 @@
+var slides = document.querySelector('.slides'),
+    slide = document.querySelectorAll('.slides li'),
+    currentIndex = 0,
+    slideCount = slide.length,
+    slideWidth = 200,
+    slideMargin = 30,
+    prevBtn = document.querySelector('.prev'),
+    nextBtn = document.querySelector('.next');
+
+makeClone();
+
+function makeClone(){
+    for (var i = 0; i < slideCount; i++){
+        //a.cloneNode(), a.cloneNode(true)>> 자신 포함 자식까지 클론 복사 true
+        var cloneSlide = slide[i].cloneNode(true);
+        cloneSlide.classList.add('clone');
+        //a.appendClid(b);
+        slides.appendChild(cloneSlide);
+    }
+    for (var i = slideCount-1; i  >= 0 ; i--){
+        //a.cloneNode(), a.cloneNode(true)>> 자신 포함 자식까지 클론 복사 true
+        var cloneSlide = slide[i].cloneNode(true);
+        cloneSlide.classList.add('clone');
+        //a.prepend(b);
+        slides.prepend(cloneSlide);
+    }
+
+    updateWidth();
+    setIntialPos();
+    setTimeout(function(){
+        slides.classList.add('animated');
+    }, 100)
+}
+
+function updateWidth(){
+    var currentSlides = document.querySelectorAll('.slides li');
+    var newSlideCount = currentSlides.length;
+    var newWidth = (slideWidth + slideMargin ) * newSlideCount - slideMargin + 'px';
+    slides.style.width = newWidth;
+}
+
+function setIntialPos(){
+    var initialTranslateValue = -(slideWidth + slideMargin ) * slideCount;
+    slides.style.transform = 'translateX('+initialTranslateValue+'px)';
+}
+
+
+//button
+prevBtn.addEventListener('click', function(){
+    moveSlide(currentIndex - 1);
+})
+nextBtn.addEventListener('click', function(){
+    moveSlide(currentIndex + 1);
+})
+
+function moveSlide(index){
+    slides.style.left = -index * (slideWidth + slideMargin ) + 'px';
+    currentIndex = index;
+
+    //nextBtn/prevBtn 끝까지 간 이후 currentIndex 0으로 left 0으로 변경하여 중앙에 다시 오도록, 유저는 인식 불가
+    if(currentIndex == slideCount || currentIndex == -slideCount){
+        setTimeout(function(){
+            slides.classList.remove('animated');
+            slides.style.left = '0px';
+            currentIndex = 0;
+        }, 500);
+        setTimeout(function(){
+            slides.classList.add('animated');
+        },600);
+    }
+}
